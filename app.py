463eff340d1257ec040e17c69a5162055e38605d
xiang58 <danielxiang0508@gmail.com>
2023-05-02 21:31:01 -0500

    NRT-72 Get last date

--- app.py
+++ app.py
@@ -5,9 +5,13 @@ con = sqlite3.connect(
     'C:\\Users\\xiang\\Desktop\\Programs\\Coding\\Database\\SQLite\\db\\new_record_tracker.db'
 )
 cur = con.cursor()
-result = cur.execute('''
+result = list(cur.execute('''
     SELECT * FROM "MyDate"
     ORDER BY "id" DESC
     LIMIT 1
-''')
-st.text(list(result))
+'''))
+
+last_date = result[0][1]
+type = result[0][2]
+last_date_n_type = 'Last date: {}; Type: {}'.format(last_date, type)
+st.text(last_date_n_type)
