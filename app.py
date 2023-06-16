bcb622f8222788f399f4c5d187f3ca420dfb85e7
xiang58 <danielxiang0508@gmail.com>
2023-06-15 21:48:32 -0500

    NRT-82 Extract constants

--- app.py
+++ app.py
@@ -1,67 +1,45 @@
 import sqlite3
 import streamlit as st
-from constants import COLORS
+from constants import html, misc, sql
 from datetime import date, datetime
 
-if 'current_date' not in st.session_state:
-    st.session_state['current_date'] = date.today()
+def main():
+    if 'current_date' not in st.session_state:
+        st.session_state['current_date'] = date.today()
 
-con = sqlite3.connect(
-    'C:\\Users\\xiang\\Desktop\\Programs\\Coding\\Database\\SQLite\\db\\new_record_tracker.db'
-)
-cur = con.cursor()
-result = list(cur.execute('''
-    SELECT * FROM "MyDate"
-    ORDER BY "id" DESC
-    LIMIT 1
-'''))
+    con = sqlite3.connect(sql.SQL_PATH)
+    cur = con.cursor()
+    result = list(cur.execute(sql.GET_LAST_DATE))
 
-last_date = result[0][1]
-type = result[0][2]
-last_date_n_type = 'Last date: {}; Type: {}'.format(last_date, type)
-st.text(last_date_n_type)
+    last_date = result[0][1]
+    type = result[0][2]
+    last_date_n_type = 'Last date: {}; Type: {}'.format(last_date, type)
+    st.text(last_date_n_type)
 
-date_types = list(cur.execute('''
-    SELECT "date_type" FROM "MyDate"
-'''))
-current_score = 0
-for item in date_types:
-    current_score += item[0]
-st.text('Current score: ' + str(current_score))
+    date_types = list(cur.execute(sql.GET_DATE_TYPE))
+    current_score = 0
+    for item in date_types:
+        current_score += item[0]
+    st.text('Current score: ' + str(current_score))
 
-last_date = datetime.strptime(last_date, '%Y-%m-%d').date()
-date_diff = (st.session_state['current_date'] - last_date).days
-background_index = date_diff
-if date_diff < 0:
-    background_index = 0
-if date_diff > 7:
-    background_index = 7
+    last_date = datetime.strptime(last_date, '%Y-%m-%d').date()
+    date_diff = (st.session_state['current_date'] - last_date).days
+    background_index = date_diff
+    if date_diff < 0:
+        background_index = 0
+    if date_diff > 7:
+        background_index = 7
 
-circle = '<div class="circle"><b>{}</b></div>'.format(date_diff)
-style = f'''
-<style>
-    .circle {{
-        width: 50px;
-        height: 50px;
-        border-radius: 50%;
-        background: {COLORS[background_index]};
-        text-align: center;
-        font-size: larger;
-        line-height: 50px;
-        color: rgb(0, 0, 0);
-        float: left;
-        margin: 25px 0 0px 50px;
-    }}
-</style>
-'''
-st.markdown(circle, unsafe_allow_html=True)
-st.markdown(style, unsafe_allow_html=True)
+    circle = html.CIRCLE.format(date_diff)
+    style = html.CIRCLE_STYLE.format(misc.COLORS[background_index])
+    st.markdown(circle, unsafe_allow_html=True)
+    st.markdown(style, unsafe_allow_html=True)
 
-new_date = st.date_input('Pick a date:', key='current_date', min_value=last_date, max_value=date.today())
-date_type = st.selectbox('Pick a type:', ('1', '0', '-1', '2'))
-if st.button('Add Date'):
-    cur.execute('''
-        INSERT INTO "MyDate" (my_date, date_type)
-        VALUES (?, ?)
-    ''', (new_date, date_type))
-    con.commit()
+    new_date = st.date_input('Pick a date:', key='current_date', min_value=last_date, max_value=date.today())
+    date_type = st.selectbox('Pick a type:', ('1', '0', '-1', '2'))
+    if st.button('Add Date'):
+        cur.execute(sql.ADD_DATE, (new_date, date_type))
+        con.commit()
+
+if __name__ == '__main__':
+    main()
---
+++ constants/html.py
@@ -0,0 +1,18 @@
+CIRCLE = '<div class="circle"><b>{}</b></div>'
+
+CIRCLE_STYLE = '''
+    <style>
+        .circle {{
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            background: {};
+            text-align: center;
+            font-size: larger;
+            line-height: 50px;
+            color: rgb(0, 0, 0);
+            float: left;
+            margin: 25px 0 0px 50px;
+        }}
+    </style>
+'''
