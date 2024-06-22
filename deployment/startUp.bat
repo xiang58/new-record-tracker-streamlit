a07267d7b76917b1dde474295951dc2a021dac20
xiang58 <danielxiang0508@gmail.com>
2024-06-22 15:41:03 -0500

    MRT-3 Disable auto start browser

--- deployment/startUp.bat
+++ deployment/startUp.bat
@@ -3,4 +3,4 @@ cd ..
 if not exist ".venv" (
     py -m venv .venv
 )
-.venv\Scripts\activate && pip install -r requirements.txt && streamlit run src\app.py
+.venv\Scripts\activate && pip install -r requirements.txt && streamlit run src\app.py --server.headless true
