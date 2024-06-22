283aa8c0fae112de07de31b6b3a95e2e34b8689f
xiang58 <danielxiang0508@gmail.com>
2024-06-22 15:30:46 -0500

    MRT-3 Change DB conn path

--- src/constants/sql.py
+++ src/constants/sql.py
@@ -1,3 +1,5 @@
+SQL_PATH = 'C:\\Users\\xiang\\Desktop\\Programs\\Coding\\Database\\SQLite\\db\\new_record_tracker.db'
+
 GET_LAST_DATE = '''
     SELECT * FROM "MyDate"
     ORDER BY "id" DESC
--- src/db_connector.py
+++ src/db_connector.py
@@ -1,11 +1,10 @@
-import os.path
 import sqlite3
+from constants import sql
 
 def db_connector(f):
     
     def _with_connection(*args, **kwargs):
-        db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'new_record_tracker.db'))
-        conn = sqlite3.connect(db_path)
+        conn = sqlite3.connect(sql.SQL_PATH)
         try:
             rv = f(conn, *args, **kwargs)
         except Exception:
