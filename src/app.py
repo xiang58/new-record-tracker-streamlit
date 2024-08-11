c6c448a0cb431b6834dc9610daac0c2da2ccc529
xiang58 <danielxiang0508@gmail.com>
2024-08-10 19:30:50 -0500

    MSP-13 Connect to DynamoDB

--- src/app.py
+++ src/app.py
@@ -1,16 +1,33 @@
 import streamlit as st
 from datetime import date
+import boto3
+from botocore.exceptions import ClientError
+
 
 import widgets as wd
 
 def main():
-    if 'current_date' not in st.session_state:
-        st.session_state['current_date'] = date.today()
+    # if 'current_date' not in st.session_state:
+    #     st.session_state['current_date'] = date.today()
 
-    wd.show_last_date()
-    wd.show_circle(st.session_state['current_date'])
-    wd.show_insert_new_rec(st.session_state['current_date'])
-    wd.show_date_heatmap()
+    # wd.show_last_date()
+    # wd.show_circle(st.session_state['current_date'])
+    # wd.show_insert_new_rec(st.session_state['current_date'])
+    # wd.show_date_heatmap()
+    
+    # Initialize DynamoDB resource
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+    # Specify the table
+    table = dynamodb.Table('new_record_tracker')
+
+    # Query the table
+    response = table.scan()
+
+    # Print the items from the response
+    st.text(response['Items'])
+    
+
 
 if __name__ == '__main__':
     main()
