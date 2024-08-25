7952a2d8a851d7b3404b3e21c6245dae120330ad
xiang58 <danielxiang0508@gmail.com>
2024-08-24 21:44:25 -0500

    MSP-13 Modify code to use DynamoDB

--- src/app.py
+++ src/app.py
@@ -1,33 +1,20 @@
-import streamlit as st
 from datetime import date
-import boto3
-from botocore.exceptions import ClientError
 
+import streamlit as st
 
 import widgets as wd
 
+
 def main():
-    # if 'current_date' not in st.session_state:
-    #     st.session_state['current_date'] = date.today()
+    if 'current_date' not in st.session_state:
+        st.session_state['current_date'] = date.today()
 
-    # wd.show_last_date()
-    # wd.show_circle(st.session_state['current_date'])
-    # wd.show_insert_new_rec(st.session_state['current_date'])
-    # wd.show_date_heatmap()
+    all_recs = wd.get_all_recs()
+    wd.show_last_date(all_recs)
+    wd.show_circle(all_recs, st.session_state['current_date'])
+    wd.show_insert_new_rec(all_recs, st.session_state['current_date'])
+    wd.show_date_heatmap(all_recs)
     
-    # Initialize DynamoDB resource
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-
-    # Specify the table
-    table = dynamodb.Table('new_record_tracker')
-
-    # Query the table
-    response = table.scan()
-
-    # Print the items from the response
-    st.text(response['Items'])
     
-
-
 if __name__ == '__main__':
     main()
--- src/widgets.py
+++ src/widgets.py
@@ -1,29 +1,51 @@
-import plotly.graph_objects as go
-import streamlit as st
 from datetime import date, datetime, timedelta
 
-from constants import html, misc, sql
-from db_connector import db_connector
+import boto3
+import plotly.graph_objects as go
+import streamlit as st
+
+from constants import html, misc
 from helper import transform_recs
 
 
-@db_connector
-def get_last_record(conn):
-    cur = conn.cursor()
-    result = list(cur.execute(sql.GET_LAST_DATE))
-    return result
+def get_all_recs():
+    # Initialize DynamoDB resource
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 
-def show_last_date():
-    last_rec = get_last_record()
-    last_date = last_rec[0][1]
-    type = last_rec[0][2]
+    # Specify the table
+    table = dynamodb.Table('new_record_tracker')
+
+    # Query the table
+    response = table.scan()
+
+    # Get the items from the response
+    items = response['Items']
+
+    # Transform to correct format
+    all_recs = [
+        [
+            int(item['date_id']), item['date_val'], int(item['date_type'])
+        ] 
+        for item in items
+    ]
+
+    # Sort all_recs based on ID
+    all_recs.sort(key=lambda x: x[0])
+
+    return all_recs
+
+
+def show_last_date(all_recs):
+    last_rec = all_recs[-1]
+    last_date = last_rec[1]
+    type = last_rec[2]
     last_date_n_type = 'Last date: {}; Type: {}'.format(last_date, type)
     st.text(last_date_n_type)
 
 
-def show_circle(current_date):
-    last_rec = get_last_record()
-    last_date = datetime.strptime(last_rec[0][1], '%Y-%m-%d').date()
+def show_circle(all_recs, current_date):
+    last_rec = all_recs[-1]
+    last_date = datetime.strptime(last_rec[1], '%Y-%m-%d').date()
     date_diff = (current_date - last_date).days
 
     background_index = date_diff
@@ -38,29 +60,33 @@ def show_circle(current_date):
     st.markdown(style, unsafe_allow_html=True)
 
 
-def show_insert_new_rec(current_date):
-    last_rec = get_last_record()
-    last_date = datetime.strptime(last_rec[0][1], '%Y-%m-%d').date()
+def show_insert_new_rec(all_recs, current_date):
+    last_rec = all_recs[-1]
+    last_date = datetime.strptime(last_rec[1], '%Y-%m-%d').date()
     new_date = st.date_input('Pick a date:', key='current_date', value=current_date, min_value=last_date, max_value=date.today())
     date_type = st.selectbox('Pick a type:', ('1', '0', '-1'))
-    st.button('Add Date', on_click=add_date, args=(new_date, date_type))
+    st.button('Add Date', on_click=add_date, args=(all_recs, new_date, date_type))
         
 
-@db_connector
-def add_date(conn, new_date, date_type):
-    cur = conn.cursor()
-    cur.execute(sql.ADD_DATE, (new_date, date_type))
+def add_date(all_recs, new_date, date_type):
+    new_id = len(all_recs) + 1
+    item = {
+        'date_id': new_id,
+        'date_val': str(new_date),
+        'date_type': date_type
+    }
+
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    table = dynamodb.Table('new_record_tracker')
+    response = table.put_item(Item=item)
+
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        st.toast("Item inserted successfully!", icon='🎉')
+    else:
+        st.toast("Error inserting item:" + str(response), icon='🚨')
 
 
-@db_connector
-def get_all_recs(conn):
-    cur = conn.cursor()
-    result = list(cur.execute(sql.GET_ALL_RECS))
-    return result
-
-
-def show_date_heatmap():
-    all_recs = get_all_recs()
+def show_date_heatmap(all_recs):
     first_day = datetime.strptime(all_recs[0][1], '%Y-%m-%d').date()
     today = datetime.today().date()
     num_days = (today - first_day).days + 1
